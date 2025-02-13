160072e4eec771a308fe418ad07ab8d0f5156623
Carlos Zamora <uriel.zamora@encora.com>
2025-02-13 10:07:24 -0600

    feat: implement thunks for adding, updating, and deleting todos with Redux Toolkit

--- frontend/src/redux/todoSlice.tsx
+++ frontend/src/redux/todoSlice.tsx
@@ -16,14 +16,63 @@ const initialState: TodoSliceState = {
 const getTodosAsync = createAsyncThunk<Todo[], void>(
   'todos/getTodosAsync',
   async (_, { rejectWithValue }) => {
-    const response = await fetch('http://localhost:9090/todos');
-
-    if (response.ok) {
+    try {
+      const response = await fetch('http://localhost:9090/todos');
       const todos: Todo[] = await response.json();
       return todos;
+    } catch {
+      return rejectWithValue('Failed to fetch todos');
     }
+  }
+);
 
-    return rejectWithValue('Failed to fetch data');
+export const addTodoAsync = createAsyncThunk<void, Todo>(
+  'todos/addTodoAsync',
+  async (todo, thunkAPI) => {
+    try {
+      await fetch('http://localhost:9090/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'applications/json' },
+        body: JSON.stringify(todo),
+      });
+
+      thunkAPI.dispatch(getTodosAsync());
+    } catch {
+      return thunkAPI.rejectWithValue('Error creating new todo');
+    }
+  }
+);
+
+export const updateTodoAsync = createAsyncThunk<void, Todo>(
+  'todos/updateTodoAsync',
+  async (todo, thunkAPI) => {
+    try {
+      await fetch('http://localhost:9090/todos/' + todo.id, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'applications/json' },
+        body: JSON.stringify(todo),
+      });
+
+      thunkAPI.dispatch(getTodosAsync());
+    } catch {
+      return thunkAPI.rejectWithValue('Error updating todo');
+    }
+  }
+);
+
+export const deleteTodoAsync = createAsyncThunk<void, Todo>(
+  'todos/deleteTodoAsync',
+  async (todo, thunkAPI) => {
+    try {
+      await fetch('http://localhost:9090/todos/' + todo.id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'applications/json' },
+      });
+
+      thunkAPI.dispatch(getTodosAsync());
+    } catch {
+      return thunkAPI.rejectWithValue('Error deleting todo');
+    }
   }
 );
 
@@ -32,13 +81,35 @@ const todoSlice = createSlice({
   initialState: initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(
-      getTodosAsync.fulfilled,
-      (state, action: PayloadAction<Todo[]>) => {
+    builder
+      .addCase(
+        getTodosAsync.fulfilled,
+        (state, action: PayloadAction<Todo[]>) => {
+          state.loading = false;
+          state.todos = action.payload;
+        }
+      )
+      .addCase(addTodoAsync.fulfilled, state => {
         state.loading = false;
-        state.todos = action.payload;
-      }
-    );
+      })
+      .addCase(updateTodoAsync.fulfilled, state => {
+        state.loading = false;
+      })
+      .addCase(deleteTodoAsync.fulfilled, state => {
+        state.loading = false;
+      })
+      .addCase(getTodosAsync.pending, state => {
+        state.loading = true;
+      })
+      .addCase(addTodoAsync.pending, state => {
+        state.loading = true;
+      })
+      .addCase(updateTodoAsync.pending, state => {
+        state.loading = true;
+      })
+      .addCase(deleteTodoAsync.pending, state => {
+        state.loading = true;
+      });
   },
 });
 
