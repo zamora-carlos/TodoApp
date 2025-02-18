8ea3a85ffafe4110dbca288cfa73115506e2ef13
Carlos Zamora <uriel.zamora@encora.com>
2025-02-18 17:42:40 -0600

    feat: update SearchForm component to re-fetch todos when the search button is clicked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import todos from './data/todos';
 function App() {
   const [showModal, setShowModal] = useState(false);
   return (
-    <main className="my-12 max-w-9/10 mx-auto">
+    <main className="my-12 w-9/10 max-w-5xl mx-auto">
       <h1 className="text-4xl font-bold text-slate-800">My todo list</h1>
       <SearchForm />
       <TodoList todos={todos} setShowModal={setShowModal} />
--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { TbTriangleInvertedFilled } from 'react-icons/tb';
+import Filter from '../types/Filter';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../redux/store';
+import { updateFilter } from '../redux/viewOptionsSlice';
+import { getTodosAsync } from '../redux/todosSlice';
 
 function SearchForm() {
-  const [SearchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [priority, setPriority] = useState('ALL');
+  const [done, setDone] = useState('ALL');
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleClickSearchButton = (evt: React.FormEvent) => {
+    evt.preventDefault();
+
+    const filter: Filter = {
+      name: searchTerm.trim() || null,
+      priority:
+        priority === 'ALL' ? null : (priority as 'LOW' | 'MEDIUM' | 'HIGH'),
+      done: done === 'ALL' ? null : done === 'DONE' ? true : false,
+    };
+
+    dispatch(updateFilter(filter));
+    dispatch(getTodosAsync());
+  };
 
   return (
     <section className="mt-4 md:mt-6">
@@ -17,7 +39,7 @@ function SearchForm() {
           <input
             type="text"
             placeholder="Search..."
-            value={SearchTerm}
+            value={searchTerm}
             onChange={evt => setSearchTerm(evt.target.value)}
             className="py-2 px-4 border border-slate-400 rounded-lg w-full lg:max-w-3xl"
           />
@@ -28,11 +50,14 @@ function SearchForm() {
             Priority
           </label>
           <div className="relative w-full sm:w-48 md:w-56 lg:w-64 group">
-            <select className="text-slate-700 py-2 pl-4 pr-8 bg-transparent border border-slate-400 rounded-lg w-full lg:w-64 browser-appearance-none">
-              <option>All</option>
-              <option>Low</option>
-              <option>Medium</option>
-              <option>High</option>
+            <select
+              className="text-slate-700 py-2 pl-4 pr-8 bg-transparent border border-slate-400 rounded-lg w-full lg:w-64 browser-appearance-none"
+              onChange={evt => setPriority(evt.target.value)}
+            >
+              <option value="ALL">All</option>
+              <option value="LOW">Low</option>
+              <option value="MEDIUM">Medium</option>
+              <option value="HIGH">High</option>
             </select>
 
             <TbTriangleInvertedFilled className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none text-slate-400 text-xs group-hover:text-slate-500 transition-colors-duration-200" />
@@ -45,18 +70,27 @@ function SearchForm() {
               State
             </label>
             <div className="relative w-full sm:w-48 md:w-56 lg:w-64 group">
-              <select className="text-slate-700 py-2 pl-4 pr-8 bg-transparent border border-slate-400 rounded-lg w-full lg:w-64 browser-appearance-none">
-                <option>All</option>
-                <option>Done</option>
-                <option>Undone</option>
+              <select
+                className="text-slate-700 py-2 pl-4 pr-8 bg-transparent border border-slate-400 rounded-lg w-full lg:w-64 browser-appearance-none"
+                onChange={evt => setDone(evt.target.value)}
+              >
+                <option value="ALL">All</option>
+                <option value="DONE">Done</option>
+                <option value="UNDONE">Undone</option>
               </select>
 
               <TbTriangleInvertedFilled className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none text-slate-400 text-xs group-hover:text-slate-500 transition-colors-duration-200" />
             </div>
           </div>
 
-          <button className="text-base text-slate-700 font-medium py-2 px-4 inline-block border border-slate-400 rounded-lg cursor-pointer w-full sm:max-w-2xs sm:ml-auto hover:bg-slate-100">
-            <p className="flex items-center gap-2 justify-center">
+          <button
+            type="submit"
+            className="text-base text-slate-700 font-medium py-2 px-4 inline-block border border-slate-400 rounded-lg cursor-pointer w-full sm:max-w-2xs sm:ml-auto hover:bg-slate-100"
+          >
+            <p
+              className="flex items-center gap-2 justify-center"
+              onClick={handleClickSearchButton}
+            >
               Search
               <FaMagnifyingGlass className="w-4 text-slate-700" />
             </p>
