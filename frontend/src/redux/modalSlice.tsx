abc93d252ad3c3beb5a31e2cce491d7974f0cd1f
Carlos Zamora <uriel.zamora@encora.com>
2025-02-18 12:44:27 -0600

    feat: implement redux slice to hide and show modal

---
+++ frontend/src/redux/modalSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type ModalState = {
+  isVisible: boolean;
+};
+
+const initialState: ModalState = {
+  isVisible: false,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    showModal: state => {
+      state.isVisible = true;
+    },
+    hideModal: state => {
+      state.isVisible = false;
+    },
+  },
+});
+
+export const { showModal, hideModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
