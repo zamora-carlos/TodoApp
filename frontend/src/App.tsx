0cfd306fd276b44dd5bce88c624453d95f13c029
Carlos Zamora <uriel.zamora@encora.com>
2025-02-21 01:47:40 -0600

    feat: include custom css styles for checkbox

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <main className="my-12 w-9/10 max-w-5xl mx-auto">
+    <main className="my-12 w-9/10 max-w-6xl mx-auto">
       <h1 className="text-4xl font-bold text-slate-800">My todo list</h1>
       <SearchForm />
       <TodoList />
