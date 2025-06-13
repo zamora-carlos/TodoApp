8d10b54a9415de5d25830e1d8c6cf713232eb08b
Carlos Zamora <uriel.zamora@encora.com>
2025-06-13 07:24:34 -0600

    feat: implement utility function to format labels

---
+++ src/utils/formatLabel.ts
@@ -0,0 +1,9 @@
+function formatLabel(value: string): string {
+  return value
+    .toLowerCase()
+    .split('_')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+export default formatLabel;
